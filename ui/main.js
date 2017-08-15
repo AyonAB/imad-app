7457387902f92954cfd346ec7b173d483b36a80a
AyonAB <30872214+AyonAB@users.noreply.github.com>
2017-08-15 19:35:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,3 +28,22 @@ button.onclick = function (){
     request.open('GET','http://ayantorres.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+
+// Submit Name
+
+var nameInput = document.getElementById('name');
+var name =  nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+  
+    // Capture the list of names and renders it
+    
+    var names = ['name1', 'name2', 'name3'];
+    var list = '';
+    for(i=0;i<names.length;i++){
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+    
+};
