bd492253413de931f7ce6cb1625b54b70fefec6a
AyonAB <30872214+AyonAB@users.noreply.github.com>
2017-08-26 14:51:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+var config = {
+    user: 'ayantorres',
+    database: 'ayantorres',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.ev.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -17,8 +25,17 @@ app.get('/article-one', function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
+var Pool = new Pool(config);
 app.get('/test-db', function(req,res){
-   
+        Pool.quary('SELECT * FROM test', function (err, result) {
+        if(err)
+        {
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send(JSON.Stringfy(result));
+        }
+    });
 });
 
 app.get('/article-two', function(req,res){
