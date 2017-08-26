bf7e6c614d35d7cfef2f1f0b1381b22975b1a7b4
AyonAB <30872214+AyonAB@users.noreply.github.com>
2017-08-26 13:36:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ app.get('/article-one', function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
+app.get('/test-db', function(req,res){
+   
+});
+
 app.get('/article-two', function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-two.html')); 
 });
