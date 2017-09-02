4f0f8862bcc7ab1e39b45dcc3057d9ec56b2836c
AyonAB <30872214+AyonAB@users.noreply.github.com>
2017-09-02 18:23:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
     database: 'ayantorres',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: process.ev.DB_PASSWORD
+    //password: process.ev.DB_PASSWORD
 };
 
 var app = express();
